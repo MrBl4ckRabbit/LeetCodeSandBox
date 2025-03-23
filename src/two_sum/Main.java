f796d10c7b57c0450f2cc8a9944e6a00aed53246
Rustam Miriusupov <mr.bl4ck.rabbit@gmail.com>
2025-03-24 00:13:58 +0300

    First task

---
+++ src/two_sum/Main.java
@@ -0,0 +1,38 @@
+package two_sum;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+
+        }
+
+        int target = scanner.nextInt();
+
+        System.out.println(Arrays.toString(twoSum(array, target)));
+
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
